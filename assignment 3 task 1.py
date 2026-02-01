9564442c30950c4e214f1f1e0b667aa4882d7c44
sankhadeep-coder <sankhadeep606@gmail.com>
2026-02-01 13:08:01 +0530

    Add files via upload

---
+++ assignment 3 task 1.py
@@ -0,0 +1,9 @@
+def f(x):
+    c=1
+    for i in range (1,x+1):
+        c=c*i
+    print(f'factorical of {x} is {c}')
+
+
+x= int(input("ent a num: "))
+f(x)
---
+++ assignment 3 task 2.py
@@ -0,0 +1,5 @@
+import math
+x= float(input("ent a num"))
+print(f'squar root is {math.sqrt(x)}')
+print(f'log is {math.log(x)}')
+print(f'sin is {math.sin(x)}')
